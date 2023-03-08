f2fd38dc7c2bc02141ca86c67486e5354b235ac9
mark bashir-ugwi <beejayphil@gmail.com>
2023-03-08 14:27:32 +0100

    added index to aid search

--- lib/data/schema/Movie.schema.ts
+++ lib/data/schema/Movie.schema.ts
@@ -2,11 +2,11 @@ import { IMovie } from '../models/IMovie';
 import mongoose from 'mongoose';
 
 const MOVIES = 'movies';
-const MovieModel = mongoose.model(
+const Movies = mongoose.model(
   MOVIES,
   new mongoose.Schema<IMovie>({
-    title: { type: String, required: true, trim: true },
-    genre: { type: String, required: true, trim: true },
+    title: { type: String, required: true, trim: true, index: true },
+    genre: { type: String, required: true, trim: true, index: true },
     description: { type: String, required: true, trim: true },
     language: { type: String, required: true, trim: true },
     released: { type: Date, required: true, trim: true },
@@ -14,4 +14,4 @@ const MovieModel = mongoose.model(
     rated: { type: String, required: true, trim: true },
   })
 );
-export {MovieModel}
+export {Movies}
